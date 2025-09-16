576468d59441d7bfee31fe04ab70e55669ad6685
Nicpic92 <nickolaspicardircm@gmail.com>
2025-09-16 07:15:36 -0400

    Update bandpage.js

--- netlify/functions/bandpage.js
+++ netlify/functions/bandpage.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg');
 
 exports.handler = async (event) => {
-    // --- FIX: Get the slug from the query string parameters ---
     const slug = event.queryStringParameters.slug;
 
     if (!slug) {
@@ -22,9 +21,10 @@ exports.handler = async (event) => {
                 contact_public_email, contact_booking_email,
                 link_website, link_spotify, link_apple_music,
                 link_youtube, link_instagram, link_facebook,
-                press_kit_enabled, id
+                press_kit_enabled, id,
+                photo_gallery -- <-- ADDED NEW FIELD
             FROM bands 
-            WHERE slug = $1 AND press_kit_enabled = TRUE`;
+            WHERE LOWER(slug) = LOWER($1) AND press_kit_enabled = TRUE`;
         
         const { rows: [band] } = await client.query(bandQuery, [slug]);
 
@@ -49,7 +49,8 @@ exports.handler = async (event) => {
                 hero_image_url: band.hero_image_url,
                 bio: band.bio,
                 contact: { public: band.contact_public_email, booking: band.contact_booking_email },
-                links: { website: band.link_website, spotify: band.link_spotify, apple_music: band.link_apple_music, youtube: band.link_youtube, instagram: band.link_instagram, facebook: band.link_facebook }
+                links: { website: band.link_website, spotify: band.link_spotify, apple_music: band.link_apple_music, youtube: band.link_youtube, instagram: band.link_instagram, facebook: band.link_facebook },
+                photo_gallery: band.photo_gallery || [] // <-- ADDED NEW FIELD
             },
             events: events
         };
